64e2e532e8a967f72b9ac41ea6c1d7821c3a684d
Avery Burne <burneav@gmail.com>
2021-11-17 22:31:57 -0500

    Added initialize board function, creates empty board

--- src/Board.js
+++ src/Board.js
@@ -6,6 +6,26 @@ class Board extends React.Component {
       gameStatus: false,
       mineCount: this.props.mines
     };
+
+    createEmptyArray(height, width) {
+        let data = [];
+        for (let i = 0; i < height; i++) {
+          data.push([]);
+          for (let j = 0; j < width; j++) {
+            data[i][j] = {
+              x: i,
+              y: j,
+              isMine: false,
+              neighbour: 0,
+              isRevealed: false,
+              isEmpty: false,
+              isFlagged: false,
+              };
+            }
+          }
+          return data;
+        }
+
   render() {
     return (
       <div className="board">
